99f93206b52dabcbe6e4e077cc44c0e532627965
qurikzz <qurikzqurikz@gmail.com>
2024-08-31 04:11:13 +0500

    ឵឵

--- bladesoccer.lua
+++ bladesoccer.lua
@@ -20,7 +20,7 @@ function lmao()
     if ball and playerGoal then
         local distanceToPlayerGoal = (ball.Position - playerGoal.Position).Magnitude
         if distanceToPlayerGoal <= 100 then
-            character:MoveTo(ball.Position - Vector3.new(0, 5, 0))
+            character:MoveTo(ball.Position - Vector3.new(0, 3, 0))
             task.wait()
             local blockButton = player.PlayerGui.Gui.ActionsContainer:FindFirstChild("BlockButton")
             if blockButton then
