914559e71551678daf45493aaea4b25cfbe80aa3
qurikzz <qurikzqurikz@gmail.com>
2025-01-03 01:19:07 +0500

    ឵឵

--- touchfootball.lua
+++ touchfootball.lua
@@ -7,6 +7,56 @@ if _G.ScriptLoaded then
 end
 _G.ScriptLoaded = true
 
+local Positions = UDim2.new(0.7, 0, 0.040, 0)
+local Sizes = UDim2.new(0, 150, 0, 50)
+
+local ms = Instance.new("ScreenGui")
+local stuffdd = Instance.new("Frame")
+local TextButton = Instance.new("TextButton")
+local UICorner = Instance.new("UICorner")
+local UIStroke = Instance.new("UIStroke")
+
+ms.Name = "hey"
+ms.Parent = game:WaitForChild("CoreGui")
+ms.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+stuffdd.Name = "1"
+stuffdd.Parent = ms
+stuffdd.BackgroundTransparency = 1
+stuffdd.Position = Positions
+stuffdd.Size = Sizes
+
+TextButton.Parent = stuffdd
+TextButton.BackgroundColor3 = Color3.new(0.231373, 0.694118, 0.988235)
+TextButton.Size = UDim2.new(1, 0, 0.800000012, 0)
+TextButton.Text = "⚽ Score"
+TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.TextScaled = true
+TextButton.Font = Enum.Font.FredokaOne
+TextButton.BorderSizePixel = 0
+TextButton.RichText = false
+TextButton.ClipsDescendants = false
+TextButton.TextStrokeTransparency = 1
+
+UICorner.CornerRadius = UDim.new(1, 0)
+UICorner.Parent = TextButton
+
+UIStroke.Parent = TextButton
+UIStroke.Color = Color3.new(0.109804, 0.270588, 0.321569)
+UIStroke.Thickness = 4.070723056793213
+UIStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+
+TextButton.MouseButton1Down:Connect(function()
+    TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    TextButton.TextColor3 = Color3.new(0.231373, 0.694118, 0.988235)
+    TextButton.Text = "Clicked"
+    Start()
+    task.wait(0.2)
+    TextButton.BackgroundColor3 = Color3.new(0.231373, 0.694118, 0.988235)
+    TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+    TextButton.Text = "⚽ Score"
+end)
+
 _G.CheckTeam = true
 _G.teamLetter = nil
 _G.IgnoreMeasuring = true
@@ -93,53 +143,3 @@ function Start()
         task.wait(0.5)
     until game.Players.LocalPlayer.PlayerGui.Status["Score".._G.teamLetter].Text == _G.Amount
 end
-
-local Positions = UDim2.new(0.7, 0, 0.040, 0)
-local Sizes = UDim2.new(0, 150, 0, 50)
-
-local ms = Instance.new("ScreenGui")
-local stuffdd = Instance.new("Frame")
-local TextButton = Instance.new("TextButton")
-local UICorner = Instance.new("UICorner")
-local UIStroke = Instance.new("UIStroke")
-
-ms.Name = "hey"
-ms.Parent = game:WaitForChild("CoreGui")
-ms.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-stuffdd.Name = "1"
-stuffdd.Parent = ms
-stuffdd.BackgroundTransparency = 1
-stuffdd.Position = Positions
-stuffdd.Size = Sizes
-
-TextButton.Parent = stuffdd
-TextButton.BackgroundColor3 = Color3.new(0.231373, 0.694118, 0.988235)
-TextButton.Size = UDim2.new(1, 0, 0.800000012, 0)
-TextButton.Text = "⚽ Score"
-TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextButton.TextScaled = true
-TextButton.Font = Enum.Font.FredokaOne
-TextButton.BorderSizePixel = 0
-TextButton.RichText = false
-TextButton.ClipsDescendants = false
-TextButton.TextStrokeTransparency = 1
-
-UICorner.CornerRadius = UDim.new(1, 0)
-UICorner.Parent = TextButton
-
-UIStroke.Parent = TextButton
-UIStroke.Color = Color3.new(0.109804, 0.270588, 0.321569)
-UIStroke.Thickness = 4.070723056793213
-UIStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-
-TextButton.MouseButton1Down:Connect(function()
-    TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    TextButton.TextColor3 = Color3.new(0.231373, 0.694118, 0.988235)
-    TextButton.Text = "Clicked"
-    Start()
-    task.wait(0.2)
-    TextButton.BackgroundColor3 = Color3.new(0.231373, 0.694118, 0.988235)
-    TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-    TextButton.Text = "⚽ Score"
-end)
